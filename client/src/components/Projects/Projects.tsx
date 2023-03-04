b5dfd9430229628f0b345e397e1fe6dd5f012007
31000s <burakyabgu@gmail.com>
2023-03-05 02:53:33 +0300

    'Projects' kısmının bitmesi ve 'Helpers.scss'

--- client/src/components/Projects/Projects.tsx
+++ client/src/components/Projects/Projects.tsx
@@ -1,4 +1,9 @@
 import React from 'react';
+import spotifyProductDesign from '../../Assets/sites/spd-desktop.png';
+import mernTodoApp from '../../Assets/sites/mern-todo-app.png';
+import github from '../../Assets/icons/github.svg';
+import openLink from '../../Assets/icons/open-link.svg';
+
 
 const Projects: React.FC = ()=>{
     return(
@@ -9,12 +14,31 @@ const Projects: React.FC = ()=>{
             </div>
 
             <div className='projects-space'>
-                <div className='project'>
+                <div className='project-left'>
+
                     <div className="project-box">
+                            <img src={spotifyProductDesign} alt="Spotify Product Design Desktop Image" />
                     </div>
                     <div className="project-text">
                         <h2>Spotify Product Design</h2>
-                        <p>A site I made myself, written in css.</p>
+                        <p>A site I made myself, written in scss.</p>
+                        <div className="project-buttons">
+                        <a href="https://github.com/iambuvak/spotify-product-design" target='_blank'><img src={github} alt="Github Logo" /></a>
+                        <a href="https://iambuvak.github.io/spotify-product-design" target='_blank'><img src={openLink} alt="Open Link" /></a>
+                        </div>
+                    </div>
+                </div>
+                <div className='project-right'>
+                    <div className="project-text">
+                        <h2>Mern Todo App</h2>
+                        <p>"Mern Todo App" is an application that<br /> I wrote with React TypeScript and Scss <br /> on the Frontend and Nodejs on the Backend with Mongodb.</p>
+                        <div className="project-buttons">
+                        <a href="https://github.com/iambuvak/mern-todo-app" target='_blank'><img src={github} alt="Github Logo" /></a>
+                        <a href="#" target='_blank'><img src={openLink} alt="Open Link" /></a>
+                        </div>
+                    </div>
+                    <div className="project-box">
+                            <img src={mernTodoApp} alt="Spotify Product Design Desktop Image" />
                     </div>
                 </div>
             </div>
